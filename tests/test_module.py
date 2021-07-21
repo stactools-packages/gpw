efc2f626bddd8f322375ea7e4a8a1243b857c3d4
Tyler Battle <tyler@battle.ca>
2021-07-20 19:29:34 -0700

    Run rename script

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,8 +1,8 @@
 import unittest
 
-import stactools.package
+import stactools.gpw
 
 
 class TestModule(unittest.TestCase):
     def test_version(self):
-        self.assertIsNotNone(stactools.package.__version__)
+        self.assertIsNotNone(stactools.gpw.__version__)
